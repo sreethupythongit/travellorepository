afefe1530936ad987c963c902ebead79723a53b9
sreethu <sreethuvmahesh@gmail.com>
2022-10-31 13:29:26 +0530

    travello

---
+++ travelproject/travelapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from travelapp import views
+
+urlpatterns = [
+    path('',views.demo,name='demo'),
+    path('about/',views.about,name='about'),
+    path('contact',views.contact,name='contact')
+]
---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def demo(request):
+    return render(request,"index.html")
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return HttpResponse("hi i am contact")
